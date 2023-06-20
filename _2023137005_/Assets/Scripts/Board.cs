9f7a86f950cc9e8134236c081c65e9f19da7adb9
mming0v0 <minji10221004@gmail.com>
2023-06-21 02:28:40 +0900

    mm

--- _2023137005_/Assets/Scripts/Board.cs
+++ _2023137005_/Assets/Scripts/Board.cs
@@ -7,14 +7,32 @@ public class Board : MonoBehaviour
 	[SerializeField]
 	private GameObject tilePrefab;                             
 	[SerializeField]
-	private Transform tilesParent;                        
+	private Transform tilesParent;   
+    
+    private	List<Tile>	tileList;
                                
 	private Vector2Int puzzleSize = new Vector2Int(4, 4);
+    private	float		neighborTileDistance = 102;
 
-    private void Start()
+    public	Vector3		EmptyTilePosition { set; get; }
+    public	int			Playtime { private set; get; } = 0;		
+	public	int			MoveCount { private set; get; } = 0;
+
+    private IEnumerator Start()
     {
+        tileList = new List<Tile>();
+
         SpawntIles();
-    
+
+        UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(tilesParent.GetComponent<RectTransform>());
+
+		yield return new WaitForEndOfFrame();
+
+        tileList.ForEach(x => x.SetCorrectPosition());
+
+        StartCoroutine("OnSuffle");
+
+        StartCoroutine("CalculatePlaytime");
     }
 
     private void SpawntIles()
@@ -23,10 +41,68 @@ public class Board : MonoBehaviour
         {
             for (int x = 0; x <puzzleSize.x; ++ x)
             {
-                Instantiate(tilePrefab, tilesParent);
+                GameObject clone = Instantiate(tilePrefab, tilesParent);
+				Tile tile = clone.GetComponent<Tile>();
+
+                tile.Setup(this, puzzleSize.x * puzzleSize.y, y * puzzleSize.x + x + 1);
+
+                tileList.Add(tile);
             }
         }
     }
+    private IEnumerator OnSuffle()
+	{
+		float current	= 0;
+		float percent	= 0;
+		float time		= 1.5f;
 
+		while ( percent < 1 )
+		{
+			current += Time.deltaTime;
+			percent = current / time;
+
+			int index = Random.Range(0, puzzleSize.x * puzzleSize.y);
+            tileList[index].transform.SetAsLastSibling();
+
+            yield return null;
+        }
+        EmptyTilePosition = tileList[tileList.Count-1].GetComponent<RectTransform>().localPosition;
+    }
+
+    public void IsMoveTile(Tile tile)
+	{
+		if ( Vector3.Distance(EmptyTilePosition, tile.GetComponent<RectTransform>().localPosition) == neighborTileDistance)
+		{
+			Vector3 goalPosition = EmptyTilePosition;
+
+			EmptyTilePosition = tile.GetComponent<RectTransform>().localPosition;
+
+			tile.OnMoveTo(goalPosition);
+
+            MoveCount ++;
+		}
+	}
+
+    public void IsGameOver()
+	{
+		List<Tile> tiles = tileList.FindAll(x => x.IsCorrected == true);
+
+		Debug.Log("Correct Count : "+tiles.Count);
+		if ( tiles.Count == puzzleSize.x * puzzleSize.y - 1 )
+		{
+			Debug.Log("GameClear");
+            StopCoroutine("CalculatePlaytime");
+            GetComponent<UIController>().OnResultPanel();
+        }
+    }
+
+    private IEnumerator CalculatePlaytime()
+	{
+		while ( true )
+		{
+			Playtime ++;
+
+			yield return new WaitForSeconds(1);
+		}
+	}
 }
-
